1cb888b480e476290a94fa49eb429a149cae36af
Fran Soto <fran-soto@hotmail.es>
2021-11-12 10:57:33 -0600

    Sample file for api keys

---
+++ CognitiveServicesSO/Keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServicesSO
+{
+    class Keys
+    {
+        public const string FACE_KEY = "<FACE-KEY>";
+        public const string FACE_ENDPOINT = "<FACE-ENDPOINT>";
+        public const string VISION_KEY = "<VISION-KEY>";
+        public const string VISION_ENDPOINT = "<VISION-ENDPOINT>";
+    }
+}
